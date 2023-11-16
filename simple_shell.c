27b63fe35f7bd446c368c977da01b33ce548e560
Amr Emad <amr12201147@gmail.com>
2023-11-16 15:50:04 -0800

    betty errors solved

--- simple_shell.c
+++ simple_shell.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 #define BUFFER_SIZE 1024
 
 /**
@@ -10,41 +9,41 @@
 
 int main(void)
 {
-    char *buffer = NULL;
-    size_t bufsize = 0;
+	char *buffer = NULL;
+	size_t bufsize = 0;
 
-    while (1)
-    {
-        ssize_t characters_read;
+	while (1)
+	{
+		ssize_t characters_read;
 
-        printf(":) ");
+		printf(":) ");
 
-        characters_read = getline(&buffer, &bufsize, stdin);
+		characters_read = getline(&buffer, &bufsize, stdin);
 
-        if (characters_read == -1)
-        {
-            printf("\n");
-            break;
-        }
+		if (characters_read == -1)
+		{
+			printf("\n");
+			break;
+		}
 
-        buffer[characters_read - 1] = '\0';
+		buffer[characters_read - 1] = '\0';
 
-        if (strcmp(buffer, "exit") == 0)
-        {
-            printf("Exiting shell\n");
-            break;
-        }
+		if (strcmp(buffer, "exit") == 0)
+		{
+			printf("Exiting shell\n");
+			break;
+		}
 
-        if (strcmp(buffer, "env") == 0)
-        {
-            print_environment();
-            continue;
-        }
+		if (strcmp(buffer, "env") == 0)
+		{
+			print_environment();
+			continue;
+		}
 
-        execute_command(buffer);
-    }
+		execute_command(buffer);
+	}
 
-    free(buffer);
+	free(buffer);
 
-    return (0);
+	return (0);
 }
